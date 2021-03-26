ea7b2f43be76f572fe7f3750da0dbae403c15ad9
Moyuri Akther <akthermoyuri@.com>
2021-03-27 05:00:46 +0600

    push

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3vRvoD3ixPLYBsL6Yz33ZW4qiaVWB4v4",
+    authDomain: "burj-al-arab-cc6b6.firebaseapp.com",
+    projectId: "burj-al-arab-cc6b6",
+    storageBucket: "burj-al-arab-cc6b6.appspot.com",
+    messagingSenderId: "732618472926",
+    appId: "1:732618472926:web:eae18a548e511edfcd314f"
+  };
+  export default firebaseConfig;
